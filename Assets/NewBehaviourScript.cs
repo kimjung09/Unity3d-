7315a3cd7c0fe2c506e37e88a7bfc404fecc74ac
kimjung09 <80080048+kimjung09@users.noreply.github.com>
2021-03-10 13:15:16 +0900

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 간단한 메세지 만들기
+        Debug.Log("Hello Unity"); //콘솔창띄우는법
+    }
+
+}
+ 
